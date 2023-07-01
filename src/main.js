53b6eaf9d7f0fc2149cf192c2b4b68ea6fa7659a
chad <1411430556@qq.com>
2023-07-01 19:59:48 +0800

    Home-product列表渲染

--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import router from './router'
 import '@/styles/common.scss'
 
 // 引入懒加载指令插件并注册
-import {lazyPlugin} from '@/directives'
+import { lazyPlugin } from '@/directives'
 
 const app = createApp(App)
 
