2097e20379ad81bf36359fc5f5f2eaeaeef0c9cb
chad <1411430556@qq.com>
2023-07-01 18:55:50 +0800

    Home-图片懒加载指令实现

--- src/apis/home.js
+++ src/apis/home.js
@@ -13,3 +13,12 @@ export function getBannerAPI () {
 export const findNewAPI = () => {
   return httpInstance.get('/home/new')
 }
+
+/**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+export const getHotAPI = () => {
+  return  httpInstance.get('home/hot')
+}
