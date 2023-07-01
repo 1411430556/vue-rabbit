f82541849db7a5bc9bac48daf90f46662b311f83
chad <1411430556@qq.com>
2023-07-01 15:21:52 +0800

    Home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import HomePanel from '@/views/Home/components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,6 +15,13 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew/>
   <HomeHot/>
   <homeProduct/>
+  <!--  测试  -->
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>111</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>222</div>
+  </HomePanel>
 </template>
 
 <style scoped>
