e3b648dc20fb69f007db8b9b12d34ccb873e02fa
chad <1411430556@qq.com>
2023-07-02 23:18:27 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -6,5 +6,14 @@ import httpInstance from '@/utils/http'
  * @returns {Promise<httpInstance.AxiosResponse<any>>}
  */
 export function getCategoryAPI (id) {
-  return httpInstance.get(`/category/?id=${id}`)
+  return httpInstance.get(`/category?id=${id}`)
+}
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id
+ * @return {*}
+ */
+export const getCategoryFilterAPI = (id) => {
+  return httpInstance.get(`/category/sub/filter?id=${id}`)
 }
