c3c970eaa44ae13d86a7f4a09710413242008a34
chad <1411430556@qq.com>
2023-07-03 17:11:20 +0800

    二级分类-基础列表渲染

--- src/apis/category.js
+++ src/apis/category.js
@@ -17,3 +17,17 @@ export function getCategoryAPI (id) {
 export const getCategoryFilterAPI = (id) => {
   return httpInstance.get(`/category/sub/filter?id=${id}`)
 }
+
+/**
+ * @description: 获取导航数据
+ * @data {
+     categoryId: 1005000 ,
+     page: 1,
+     pageSize: 20,
+     sortField: 'publishTime' | 'orderNum' | 'evaluateNum'
+   }
+ * @return {*}
+ */
+export const getSubCategoryAPI = (data) => {
+  return httpInstance.post('/category/goods/temporary', data)
+}
